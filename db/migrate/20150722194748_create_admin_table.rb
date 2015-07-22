5be995a211931aaa02222a3bb1c81938d4dd19a2
Timothy <t.williams.im@gmail.com>
2015-07-22 16:03:21 -0400

    migrated tables

--- db/migrate/20150722194748_create_admin_table.rb
+++ db/migrate/20150722194748_create_admin_table.rb
@@ -1,9 +1,10 @@
 class CreateAdminTable < ActiveRecord::Migration
   def change
     create_table :admins do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :full_name
       t.string :email
+      t.string :password
       t.timestamps null: false
+    end
   end
 end
