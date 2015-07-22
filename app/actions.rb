b156b6aaa7b9406d21f8a8f8f80917f53ea81761
Timothy <t.williams.im@gmail.com>
2015-07-22 16:49:42 -0400

    web authentification

--- app/actions.rb
+++ app/actions.rb
@@ -9,5 +9,11 @@ end
 post '/verify_login' do
   admin_email = params[:admin_email]
   password = params[:password]
+  current_admin = Admin.find_by email: admin_email
+  if current_admin && current_admin.authenticate(password)
+    session["admin_id"] = current_admin.id
+    puts session["admin_id"]
+  end
   redirect '/calendar'
+
 end
