1138e9f6958c713bcb96a4251db27e1853d36320
LaithAzer <laith.azer@gmail.com>
2015-07-23 16:53:57 +0000

    tests admin authentication

--- spec/admin_test.rb
+++ spec/admin_test.rb
@@ -1,32 +1,48 @@
 require './spec/spec_helper'
 
-describe "#new" do 
+describe "admin_tests" do 
 
   before :all do
     Admin.create(full_name: "test admin", email: "test@test.com", password: '123', password_confirmation: '123')   
     @admin = Admin.last
   end
 
-  it "should have a full name" do 
-    expect(@admin.full_name).to eq("test admin")
+  describe "#new" do 
+
+    it "should have a full name" do 
+      expect(@admin.full_name).to eq("test admin")
+    end
+
+    it "should not store the password" do 
+      expect(@admin.password).to be_falsey
+    end
+
+    it "should not save if password confirmation doesnt match" do 
+      Admin.create(full_name: "test admin2", email: "test2@test.com", password: '123', password_confirmation: '321')
+      expect(Admin.last.full_name).to eq("test admin")
+    end
+
+    it "should not allow duplicate emails" do 
+      Admin.create(full_name: "test admin2", email: "test@test.com", password: '123', password_confirmation: '123')
+      expect(Admin.last.full_name).to eq("test admin")
+    end
+
   end
 
-  it "should not store the password" do 
-    expect(@admin.password).to be_falsey
+  describe "#authentication" do 
+
+    it "should fail if password is wrong" do 
+      expect(@admin.authenticate("321")).to be_falsey
+    end
+
+    it "should return true if password is correct" do 
+      expect(@admin.authenticate("123")).to be_truthy
+    end
+
   end
 
-  it "should not save if password confirmation doesnt match" do 
-    Admin.create(full_name: "test admin2", email: "test2@test.com", password: '123', password_confirmation: '321')
-    expect(Admin.last.full_name).to eq("test admin")
-  end
-
-  it "should not allow duplicate emails" do 
-    Admin.create(full_name: "test admin2", email: "test@test.com", password: '123', password_confirmation: '123')
-    expect(Admin.last.full_name).to eq("test admin")
-  end
-
-  after :all do 
-    @admin.destroy
-  end
+    after :all do 
+      @admin.destroy
+    end
 
 end
