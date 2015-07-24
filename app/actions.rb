864bf8ae477cccb7895a569f4d314abd840ae3f3
Timothy <t.williams.im@gmail.com>
2015-07-24 09:06:00 -0400

    modified views etc

--- app/actions.rb
+++ app/actions.rb
@@ -6,6 +6,25 @@ get '/' do
   erb :'index'
 end
 
+get '/shifts' do
+  @shifts = Shift.all
+  erb :'shifts/index'
+end
+
+get '/calendar' do
+  erb :'/calendar/index'
+end
+
+post '/verify_login' do
+  @admin = Admin.find_by email: params[:email]
+  if @admin && @admin.authenticate(params[:password])
+    session[:user] = @admin
+    redirect '/calendar'
+  else
+    redirect '/authentification_failed'
+  end
+end
+
 get '/mentors' do
   @mentors = Mentor.all
   erb :'mentors/index'
@@ -32,29 +51,13 @@ get '/mentors/:id/shifts' do
   erb :'mentors/show/shifts'
 end
 
-
-get '/shifts' do
-  @shifts = Shift.all
-  erb :'shifts/index'
-end
-
 post '/mentors/:id/shifts' do
-
   redirect '/mentors/:id/shifts'
 end
 
-get '/calendar' do
-  erb :'/calendar/index'
-end
-
-post '/verify_login' do
-  @admin = Admin.find_by email: params[:email]
-  if @admin && @admin.authenticate(params[:password])
-    session[:user] = @admin
-    redirect '/calendar'
-  else
-    redirect '/authentification_failed'
-  end
+get '/mentors/:id/shifts/new' do
+  @mentor = Mentor.find(params[:id])
+  erb :'/shifts/new'
 end
 
 
