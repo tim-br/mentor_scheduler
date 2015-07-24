2424601bde77248baffb8cec9a7dd3ac6f99e155
LaithAzer <laith.azer@gmail.com>
2015-07-24 20:15:42 +0000

    gets random first schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -5,16 +5,21 @@ class Schedule
   def initialize
     Shift.all.each do |shift|
       count = 0
-      mentor_array = Mentor.all
+      mentor_array = []
+      # mentor_array = Mentor.all
+      Mentor.all.each do |mentor|
+        mentor_array << mentor
+      end
       begin
         count+=1
-        shift.mentor = mentor_array.sample
-        if shift.mentor.check_if_available?(shift)                
+        sample_mentor = mentor_array.sample
+        if sample_mentor.check_if_available?(shift)              
+          shift.mentor = sample_mentor
           shift.save
         end                
-        mentor_array.delete(shift.mentor)
+        mentor_array.delete(sample_mentor)
       end while(shift.mentor.nil? && count<20)
-      puts "no mentor found" if shift.mentor.nil?
+      puts "no mentor found for shift #{shift.id}" if shift.mentor.nil?
     end
   end
 
