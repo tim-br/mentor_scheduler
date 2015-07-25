7b621a9f3fb2dd8c39a10b17aa84a25e49111a12
LaithAzer <laith.azer@gmail.com>
2015-07-25 03:09:31 +0000

    optimizes but initial solution poor

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,16 +1,16 @@
-require './config/environment'
+#require './config/environment'
 
 class Schedule
 
-  attr_accessor :shifts
+  attr_accessor :shifts, :score
 
   def initialize
     @shifts = []
     mentor_array = Mentor.all.to_a
     Shift.all.each do |shift|
       mentor_array_copy = mentor_array.dup
+      assigned = nil
       begin
-        assigned = nil
         sample_mentor = mentor_array_copy.sample
         if sample_mentor.check_if_available?(shift)              
           shift.mentor = sample_mentor
@@ -24,29 +24,22 @@ class Schedule
     end
   end
 
-  # def score
-  #   @score = 1000
-  #   #valid?: Is every shift filled?
-  #   unless self.valid?
-  #     return 0
-  #   end
-  #   #has every TA been assigned a shift?
-  #   #does a TA have gaps in their shifts on the same day?
-  #   Mentor.each do |mentor|
-  #     mentor_shifts = self.shifts.select {|shift| shift.mentor == mentor}
-  #     if mentor_shifts.length == 0
-  #       @score -= 1
-  #     else
-  #       mentor_shifts_days = (mentor_shifts.map &:day).uniq
-  #       mentor_shifts_days.each |day| do 
-  #         shift_start_hours = mentor_shifts.select {|shift| shift.day = day}
-
-  #       end
-
-  #     end
-  #   end
-
-  # end
+  def score_calculator
+    self.score = 100
+    #valid?: Is every shift filled?
+    unless self.valid?
+      return 0
+    end
+    #has every TA been assigned a shift?
+    #does a TA have gaps in their shifts on the same day?
+    for i in (1..5) do 
+      Mentor.all.each do |mentor| 
+        self.score-=shift_gap_checker(mentor, i)
+      end
+    end
+    return self.score
+    #are any constraints broken?
+  end
 
   def valid?
     self.shifts.select {|shift| shift.mentor_id == nil }.length == 0
@@ -62,18 +55,17 @@ class Schedule
     count
   end
 
-  def shift_gap_checker
-    array_1 = []
-    Mentor.all.each do |mentor|
-      mentor_shifts = self.shifts.select {|shift| shift.mentor == mentor}
-      mentor_shifts_days = (mentor_shifts.map &:day).uniq.sort
-      mentor_shifts_days.each do |day| 
-        shifts_of_day = mentor_shifts.select {|shift| shift.day == day}
-        shift_hours = hours((shifts_of_day.map &:hour).sort)
-        array_1 << "On day #{day}, mentor #{mentor.full_name} has #{shift_hours} hours of gaps" 
-      end
-    end
-    return array_1
+  def shift_gap_checker(mentor, day)
+    shift_hours = 0
+    mentor_shifts = self.shifts.select {|shift| shift.mentor == mentor}
+    mentor_shift_days = mentor_shifts.select {|shift| shift.day == day}
+    # mentor_shifts_days = (mentor_shifts.map &:day).uniq.sort
+    # mentor_shifts_days.each do |day| 
+    #   shifts_of_day = mentor_shifts.select {|shift| shift.day == day}
+    # shift_hours += hours((shifts_of_day.map &:hour).sort)
+    shift_hours += hours((mentor_shift_days.map &:hour).sort)
+    # end
+    # return shift_hours
   end
 
 end
--- app/models/simulated_anneal.rb
+++ app/models/simulated_anneal.rb
@@ -1,4 +1,4 @@
-require '../.././config/environment'
+#require '../.././config/environment'
 
 class SimulatedAnneal 
 
@@ -9,14 +9,42 @@ class SimulatedAnneal
     @best_solution = DeepClone.clone first_solution
   end
 
-  def optimize
-    # self.new_solution = DeepClone.clone self.current_solution
+  def optimize(temp, cooling_rate)
+    while(temp>0.1)
+      self.new_solution = DeepClone.clone self.current_solution
+      # temp = 10
+      # cooling_rate = 0.03
+      #switch two random TA assignments, not caring about constraints
+      shift_position_1 = rand(0...Shift.all.count)
+      shift_position_2 = rand(0...Shift.all.count)
+
+      temp_mentor = self.new_solution.shifts[shift_position_1].mentor
+      self.new_solution.shifts[shift_position_1].mentor = self.new_solution.shifts[shift_position_2].mentor
+      self.new_solution.shifts[shift_position_2].mentor = temp_mentor
+
+      energy = self.best_solution.score_calculator
+      new_energy = self.new_solution.score_calculator
+
+      if new_energy > energy
+        self.best_solution = DeepClone.clone self.new_solution
+      elsif acceptance_probability(energy, new_energy, temp) > rand
+        self.current_solution = DeepClone.clone self.new_solution
+      end
+      temp *= (1-cooling_rate)
+    end
+  end
+
+  def acceptance_probability(energy, new_energy, temp)
+    # (new_energy - energy) / temp
+    1/( 1 + ( (energy - new_energy ) / temp ) )
   end
 
 end
 
 
-s = Schedule.new
+# s = Schedule.new
+# sa = SimulatedAnneal.new(s)
+# sa.optimize
 #have to randomly assign shifts that 'work'
 # 4 mentors
 # constraint1 = mentor1, can only work after 5pm
